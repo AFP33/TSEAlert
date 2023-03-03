4d77fef422ebcfc1f4a94022303738f8d036eda7
afp33 <afp33dev@gmail.com>
2023-03-03 18:29:46 +0330

    Update AssemblyInfo

--- src/TSEAlert/Properties/AssemblyInfo.cs
+++ src/TSEAlert/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StockExchangeAlert")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TSE Alert")]
+[assembly: AssemblyDescription("Tehran Securities Exchange Alert")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StockExchangeAlert")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("https://github.com/AFP33/TSEAlert/")]
+[assembly: AssemblyProduct("TSE Alert")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-BM")]
